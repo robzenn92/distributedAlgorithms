2ceb70dd1cdedb4df03ab4e706773b2fc04b9c01
Michele <michelebof93@gmail.com>
2016-08-10 19:32:05 +0200

    Inizio creazione doc

--- src/main/java/com/distributedalgorithms/project/ProcessVertex.java
+++ src/main/java/com/distributedalgorithms/project/ProcessVertex.java
@@ -1,18 +1,48 @@
 package com.distributedalgorithms.project;
 
+import java.util.Vector;
+
 /**
  * Created by Roberto on 09/08/16.
  */
-class ProcessVertex {
+class ProcessVertex implements Comparable<ProcessVertex>{
 
-    private String name = "";
+    private String peer0 = "";
+    private String peer1 = "";
+    private Vector<ProcessVertex> parent = new Vector<ProcessVertex>();
 
-    public ProcessVertex(String name) {
-        this.name = name;
+    public ProcessVertex(String p0) {
+        this.peer0 = p0;
+    }
+
+    public ProcessVertex(String p0, String p1) {
+        this.peer0 = p0;
+        this.peer1 = p1;
+    }
+
+    public String getFirst(){
+        return this.peer0;
+    }
+
+    public String getSecond(){
+        return this.peer1;
+    }
+
+    public void addParent(ProcessVertex pv){
+        parent.add(pv);
     }
 
     @Override
     public String toString() {
-        return this.name;
+        return this.peer0+this.peer1;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return peer0.equals(((ProcessVertex)obj).getFirst()) && peer1.equals(((ProcessVertex)obj).getSecond());
+    }
+
+    public int compareTo(ProcessVertex pv) {
+        return Integer.parseInt(this.toString()) - Integer.parseInt(pv.toString());
     }
 }
